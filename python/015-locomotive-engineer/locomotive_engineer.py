9c0d33e7cf42a784d23f007944404dd0a03cebc1
Wladimir Blaise <wld.world@tuta.io>
2026-06-03 12:53:42 +0200

    add python/015-locomotive-engineer

---
+++ python/015-locomotive-engineer/locomotive_engineer.py
@@ -0,0 +1,53 @@
+"""Functions which helps the locomotive engineer to keep track of the train."""
+
+
+def get_list_of_wagons(*args):
+    """Return a list of wagons.
+
+    :param: arbitrary number of wagons.
+    :return: list - list of wagons.
+    """
+    return [*args]
+
+
+def fix_list_of_wagons(each_wagons_id, missing_wagons):
+    """Fix the list of wagons.
+
+    :param each_wagons_id: list - the list of wagons.
+    :param missing_wagons: list - the list of missing wagons.
+    :return: list - list of wagons.
+    """
+    [a, b, _, *rest] = each_wagons_id
+    return [1] + missing_wagons + rest + [a, b]
+
+
+def add_missing_stops(dict_route, **kwargs):
+    """Add missing stops to route dict.
+
+    :param route: dict - the dict of routing information.
+    :param: arbitrary number of stops.
+    :return: dict - updated route dictionary.
+    """
+    dict_route["stops"] = [*kwargs.values()]
+    return dict_route
+
+
+def extend_route_information(route, more_route_information):
+    """Extend route information with more_route_information.
+
+    :param route: dict - the route information.
+    :param more_route_information: dict -  extra route information.
+    :return: dict - extended route information.
+    """
+    extended_information = {**route, **more_route_information}
+    return extended_information
+
+
+def fix_wagon_depot(wagons_rows):
+    """Fix the list of rows of wagons.
+
+    :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
+    :return: list[list[tuple]] - list of rows of wagons.
+    """
+    [[a, b, c], [d, e, f], [g, h, i]] = wagons_rows
+    return [[a, d, g], [b, e, h], [c, f, i]]
