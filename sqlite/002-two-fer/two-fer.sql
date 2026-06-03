036038da135bc58ab43187fdd966c4dd553d8830
Wladimir Blaise <wld.world@tuta.io>
2026-06-03 13:00:08 +0200

    add sqlite/002-two-fer

---
+++ sqlite/002-two-fer/two-fer.sql
@@ -0,0 +1,7 @@
+-- Schema: CREATE TABLE "twofer" ("input" TEXT, "response" TEXT);
+-- Task: update the twofer table and set the response based on the input.
+UPDATE twofer
+SET "response" = CASE
+  WHEN "input" != '' THEN 'One for ' || "input" || ', one for me.'
+  ELSE 'One for you, one for me.'
+END;
