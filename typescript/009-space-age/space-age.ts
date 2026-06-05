0be6eb0c24fd0129ba42a1cc913c749cc5a7ad3a
Wladimir Blaise <wld.world@tuta.io>
2026-06-05 21:55:53 +0200

    add typescript/009-space-age

---
+++ typescript/009-space-age/space-age.ts
@@ -0,0 +1,19 @@
+type Planet = 'mercury' |'venus' |'earth' |'mars' |'jupiter' |'saturn' |'uranus' |'neptune'
+const YEARS: Record<Planet, number> = {
+'mercury': 0.2408467,
+'venus': 0.61519726,
+'earth': 1.0,
+'mars': 1.8808158,
+'jupiter': 11.862615,
+'saturn': 29.447498,
+'uranus': 84.016846,
+'neptune': 164.79132
+}
+
+function ageEarth(seconds: number): number {
+  return seconds / 31557600;
+}
+
+export function age(planet: Planet, seconds: number): number {
+  return Math.round(100 * (ageEarth(seconds)) / (YEARS[planet])) / 100;
+}
